2c6f23b5ebc4a9f8f8aa07f1c175bb4fc3f74f73
shajib <sunhailshajib1@gmail.com>
2022-04-17 17:27:27 +0600

    Default value set

--- index.js
+++ index.js
@@ -3,31 +3,34 @@
 
 
 
-const taka=(name,year) => {
-(year>=18) ? console.log(`Hi ${name}.tomar biyer boyos hoyese. `) : console.log("boyos hoini.");
-
-
+const taka=(name ,year) => {
+(year>=18) ? console.log(`Hi ${name ?? "shajib"}.tomar biyer boyos hoyese. `) : console.log("boyos hoini.");
 
 }
 
-let name="shajib";
-let year=30;
+let name=prompt("whats your name");
+let year=prompt("whats your age");
 
 taka(name,year);
+
+
+// For while and do while
+
 
 // for (let i=0; i<10; i++){
 //     console.log(i)
 // }
+
+
 // let i=0;
 // while(i<20){
 //     console.log(i)
 //     i++
 // }
 
-do{
-    let i=0;
-}
-while(i<20){
-    console.log(i)
-    i++
-}
+//   let i=1;
+// do{
+//    console.log(i)
+//     i++
+// }
+// while(i<20);
